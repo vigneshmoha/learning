00180b378769a3cc70499a955f587764f9d5c179
Mohanavigneshwaran Annasamy <Mohanavigneshwaran.Annasamy@adp.com>
2019-02-26 20:03:32 +0530

    Add project files.

---
+++ Learning.Console.Games/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning.Console.Games
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
