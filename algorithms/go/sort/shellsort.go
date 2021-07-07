cd8075735af20363afd55ea66e59606367d1d08f
Mohan Annasamy <vigneshmoha@gmail.com>
2021-07-08 00:59:10 +0530

    feat: added shell sort in go

---
+++ algorithms/go/sort/shellsort.go
@@ -0,0 +1,13 @@
+package sort
+
+func ShellSort(array []int) []int {
+	for d:=int(len(array) / 2); d > 0; d /= 2 {
+		for (i:=d; i<len(array); i++) {
+			for(j := i; j >= d && array[j-d] > array[j]; j -= d) {
+				array[j], array[j-d] = array[j-d], array[j]
+			}
+		}
+	}
+
+	return array
+}
