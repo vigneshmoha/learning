c5154d0e9c0bca4797acc0d6c3dcfd1439b65f06
Mohan Annasamy <vigneshmoha@gmail.com>
2021-07-08 00:50:13 +0530

    chore: intialized repo with algorithms in go

---
+++ algorithms/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/vigneshmoha/learning/algorithms/go
+
+go 1.16
---
+++ algorithms/go/sort/quicksort.go
@@ -0,0 +1,28 @@
+package sort
+
+func QuickSort(array []int)[]int {
+	if len(array) <= 1 {
+		return array
+	}
+
+	q := partition(array)
+	QuickSort(array[:q])
+	QuickSort(array[q+1:])
+	return array
+}
+
+func partition(array []int) int {
+	lastindex := len(array) - 1
+	lastelement := array[lastindex]
+	index := -1
+
+	for j:=0; j < lastindex: j++ {
+		if array[j] <= lastelement {
+			index++
+			array[index], array[j] = array[j], array[index]
+		}
+	}
+
+	array[i+1], array[lastindex] = array[lastindex], array[index+1]
+	return index + 1
+}
